19d37ab9a4ca90b97bcc57a418535cc680dd3212
Nupur Dange <nupurdange16@gmail.com>
2026-07-07 15:38:50 +0000

    variables done

--- .github/01_Basics/variables.js
+++ .github/01_Basics/variables.js
@@ -1,1 +1,14 @@
 console.log("hello World");
+
+const accountId = 144553
+let accountEmail = "nupur@google.com"
+var accountCity = "jaipur"
+
+//accountId=2 ----not allowed
+
+accountEmail = "nd@gmail.com"
+accountPassword = '2121'
+accountCity = "Benguluru"  // avoid such practice
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountPassword]);
