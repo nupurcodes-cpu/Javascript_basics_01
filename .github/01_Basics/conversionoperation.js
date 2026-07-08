af0b02a0edcd33ab5c142ac092a9500fd95d583d
Nupur Dange <nupurdange16@gmail.com>
2026-07-08 14:41:55 +0000

    Datatype conversion

---
+++ .github/01_Basics/conversionoperation.js
@@ -0,0 +1,23 @@
+let age = 22
+ let convert = String(age)
+ console.log(convert)   //22
+ console.log(typeof convert);   //String
+
+ let score = "Nupur"
+ let convert2 = Number(score) 
+ console.log(convert2);   //NaN  (We can convert String to number , but it cant show how to write it in Number so it shows NaN)
+ console.log(typeof convert2) //Number  (NaN datatype => Number   )
+
+ let city = "Jaipur"
+ let c4 = Boolean(city)
+ console.log(c4)   //true
+
+ let name = 1
+ let c3 = Boolean(name)
+console.log(c3);   //true
+
+/*
+1 = true; 0 = false;
+"" = false;
+"Nupur" = true;
+*/
