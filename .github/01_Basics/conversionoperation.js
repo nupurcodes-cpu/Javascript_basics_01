42dfcb457c9ade1b8d270dc154edc2d70c5bc38b
Nupur Dange <nupurdange16@gmail.com>
2026-07-08 15:21:27 +0000

    operations

--- .github/01_Basics/conversionoperation.js
+++ .github/01_Basics/conversionoperation.js
@@ -1,4 +1,4 @@
-let age = 22
+/*let age = 22
  let convert = String(age)
  console.log(convert)   //22
  console.log(typeof convert);   //String
@@ -6,7 +6,7 @@ let age = 22
  let score = "Nupur"
  let convert2 = Number(score) 
  console.log(convert2);   //NaN  (We can convert String to number , but it cant show how to write it in Number so it shows NaN)
- console.log(typeof convert2) //Number  (NaN datatype => Number   )
+ console.log(typeof convert2) //Number  (NaN datatype => Number)
 
  let city = "Jaipur"
  let c4 = Boolean(city)
@@ -21,3 +21,41 @@ console.log(c3);   //true
 "" = false;
 "Nupur" = true;
 */
+
+
+// ***********************Operations******************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = " nupur"
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)  //122 ( "" / String is there in starting only then the whole input is treated as String)
+// console.log(1 + 2 + "2")  //32 
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+                    
